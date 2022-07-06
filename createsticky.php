79d095c7bf09c8f966c6d87b66527a1286c204d9
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-05 19:29:22 -0700

    changed the button

--- createsticky.php
+++ createsticky.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Example</title>
+        <title>creatingstickynotes</title>
     </head>
     <body>
-        <p>This is an example of a simple HTML page with one paragraph.</p>\
+        <p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. 
+            These can be the offender, online community members, moderators, etc. .</p>\
+        <br/>
+        <p>q3) Who do you think can help you address the harm?
+        q4) Who do you think has responsibility for helping you address the harm?
+        </p>
+        <br/>
+        <p> For q3 and q4, write down the name of the stakeholders on the sticky notes below. </p>
+
+        
+
+
         <button style="float:right; margin-top:15%"  onclick="window.location.href = './stickynote.php'">next</button>
 
     </body>
