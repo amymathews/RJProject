a171678659da882a54d41d77b840360979a4d48d
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-05 18:55:21 -0700

    changed the button

--- createsticky.php
+++ createsticky.php
@@ -4,8 +4,9 @@
         <title>Example</title>
     </head>
     <body>
-        <p>This is an example of a simple HTML page with one paragraph.</p>
+        <p>This is an example of a simple HTML page with one paragraph.</p>\
+        <button style="float:right; margin-top:15%"  onclick="window.location.href = './stickynote.php'">next</button>
+
     </body>
-    <button style="float:right; margin-top:15%"  onclick="window.location.href = './stickynote.php'">next</button>
 
 </html>
