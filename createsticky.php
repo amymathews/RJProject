a34b45d8d7760ac6388d6463cc1de40ece77f64e
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-06 13:21:25 -0700

    changed the button

--- createsticky.php
+++ createsticky.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>creatingstickynotes</title>
-    </head>
+	<meta charset="utf-8">
+	<title> Sense_making </title>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/d3.v6.min.js"></script>
+	<script src="js/FileSaver.js"></script>
+
+	<link rel="stylesheet" href="css/w3.css">
+	<link rel="stylesheet" href="css/style.css">
+ </head>
     <body>
         <p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused.</p>
         <p>   These can be the offender, online community members, moderators, etc. </p>
@@ -12,8 +19,20 @@
         <br/>
         <p> For q3 and q4, write down the name of the stakeholders on the sticky notes below. </p>
 
-        
-
+        <div class="content">
+			<div class="container">
+				<div id='selector' style="position: absolute;font-size:15px;width:20%">
+					<form>
+						<br><br>
+						<p id="content">Please input the content you want to display on the note</p>
+						<input id="text_on_note">
+						<br><br>
+						<button id="create" type="button">create new note</button>
+						<p id="remove_note">Remove: double click on the sticky note to remove it</p>
+					</form>
+				</div>
+			</div>
+		</div>
 
         <button style="float:right; margin-top:15%"  onclick="window.location.href = './stickynote.php'">next</button>
 
