9fe9009bb47a144d8ded09a32d3d5cf96ca89a61
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-06 17:57:29 -0700

    add note characteristics on main.css file

--- createsticky.php
+++ createsticky.php
@@ -5,20 +5,20 @@
 		<meta http-eqiv = "X-UA-Compatible" content= "IE=edge">
 		<meta name="viewport" content="width=device-width,intial-scale=1.0">
         <title>Sense_makings</title>
-		
+
 		<link rel = "stylesheet" href = "phptest/main.css">
 		<script src = "phptest/main.js" defer></script>
     </head>
     <body>
 		<div id = "app">
-			<p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused.</p>
+			<!-- <p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused.</p>
 			<p>   These can be the offender, online community members, moderators, etc. </p>
 			<br/>
 			<p>q3) Who do you think can help you address the harm?   </p>
 			<p>q4) Who do you think has responsibility for helping you address the harm?</p>
 			<br/>
 			<p> For q3 and q4, write down the name of the stakeholders on the sticky notes below. </p>
-		
+		 -->
 			<textarea class = "note">Some sample text</textarea>
 			<textarea class = "note">Some sample text</textarea>
 			<textarea class = "note">Some sample text</textarea>
