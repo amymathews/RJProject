afbb55697ae4e8937a37c8dab9c1d992fd2bfe74
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-07 10:09:13 -0700

    added note1,note2,note3 kept addnotr same

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const notesContainer = document.getElementById("app");
+const notesContainer = document.getElementsByClassName("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
 
 getNotes().forEach((note) => {
