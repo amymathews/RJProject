7aaa8e0579b88beec38f6bb9fd4c70ab3a2b348b
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-06 18:52:27 -0700

    add note characteristics on main.css file

--- createsticky.php
+++ createsticky.php
@@ -10,14 +10,14 @@
 		<script src = "./main.js" defer></script>
     </head>
     <body>
-		<div id = "app">
-			<p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused.</p>
+	<p>Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused.</p>
 			<p>   These can be the offender, online community members, moderators, etc. </p>
 			<br/>
 			<p>q3) Who do you think can help you address the harm?   </p>
 			<p>q4) Who do you think has responsibility for helping you address the harm?</p>
 			<br/>
 			<p> For q3 and q4, write down the name of the stakeholders on the sticky notes below. </p>
+		<div id = "app">
 		
 			<textarea class = "note">Some sample text</textarea>
 
