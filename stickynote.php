dca9813e76b56f5d65f3c5624781a21013e24005
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-25 11:23:16 -0700

    changed the text

--- stickynote.php
+++ stickynote.php
@@ -23,9 +23,10 @@
 				<form>
 					<p> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc.</p>
 					<p id="note_type"> Answer the following questions: </p>
+					
 					<label for="notetype">Question</label>
 					<select name="notes" id="notetype">
-						<option value="stakeholder">3</option>
+						<option value="stakeholder">Who do you think</option>
 						<option value="feeling">4</option>
 						<option value="action">5</option>
 					</select>
