8f7202c821be165c761f450c3a8661493580f113
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-25 10:58:11 -0700

    added all of new changes from Jingshu

--- stickynote.php
+++ stickynote.php
@@ -22,12 +22,12 @@
 			<div id='selector' style="position: absolute;font-size:15px;width:20%">
 				<form>
 					<p> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc.</p>
-					<p id="note_type">Please choose the note type (stakeholder, feeling, or action)</p>
-					<label for="notetype">Choose a type:</label>
+					<p id="note_type">Answer the following questions: </p>
+					<label for="notetype">Question</label>
 					<select name="notes" id="notetype">
-						<option value="stakeholder">q3</option>
-						<option value="feeling">q4</option>
-						<option value="action">q5</option>
+						<option value="stakeholder">3</option>
+						<option value="feeling">4</option>
+						<option value="action">5</option>
 					</select>
 					<br><br>
 					<p id="question">Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm?</p>
