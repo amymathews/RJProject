77147827fff57f9e55112c35a7b52b485c55d6bc
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-25 10:59:39 -0700

    changed the text

--- stickynote.php
+++ stickynote.php
@@ -22,7 +22,7 @@
 			<div id='selector' style="position: absolute;font-size:15px;width:20%">
 				<form>
 					<p> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc.</p>
-					<p id="note_type">Answer the following questions: </p>
+					<p id="note_type"> Answer the following questions: </p>
 					<label for="notetype">Question</label>
 					<select name="notes" id="notetype">
 						<option value="stakeholder">3</option>
