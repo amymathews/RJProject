ecd4588ec02e7a930c06946dc54f1d6eef3968ad
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-25 13:55:38 -0700

    changed the text

--- stickynote.php
+++ stickynote.php
@@ -21,8 +21,8 @@
 			<div id='stickynotes'></div>
 			<div id='selector' style="position: absolute;font-size:15px;width:20%">
 				<form>
-					<p> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc.</p>
-					<p id="note_type">Please answer each of the question below by toggling the drop down box: </p>
+					<p> <em> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc. <em></p>
+					<p id="note_type"> <strong> Please answer each of the question below by toggling the drop down box: </strong></p>
 					<label for="notetype">Choose a question</label>
 					<select name="notes" id="notetype">
 						<option value="stakeholder">q3</option>
@@ -30,7 +30,7 @@
 						<option value="action">q5</option>
 					</select>
 					<br><br>
-					<p id="question"> <em> q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm? <em> </p>
+					<p id="question">  q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm?  </p>
 					<p id="content">Please input the content you want to display on the note</p>
 					<input id="text_on_note">
 					<br><br>
