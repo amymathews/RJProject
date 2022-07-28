8c5ff7d5a0aad310632432d8c64fe7272a537ffe
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-27 18:06:13 -0700

    started on php backend

--- stickynote.php
+++ stickynote.php
@@ -1,6 +1,14 @@
 
 <!DOCTYPE html>
 <head>
+<meta charset="utf-8">
+    <title> Sense_making </title>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/d3.v6.min.js"></script>
+    <script src="js/FileSaver.js"></script>
+
+    <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 	<body>
 			<form  method="POST" >
