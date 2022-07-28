025a9971bad9521bea43ae563ccb54cd76ebecca
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-27 18:03:41 -0700

    started on php backend

--- stickynote.php
+++ stickynote.php
@@ -1,13 +1,9 @@
 
 <!DOCTYPE html>
 <head>
-	<meta charset="utf-8">
-	<title> Sense_making </title>
-	<link rel="stylesheet" href="css/w3.css">
-	<link rel="stylesheet" href="css/style.css">
 </head>
 	<body>
-			<form  method="POST" action = "<?php $_PHP_SELF ?>" >
+			<form  method="POST" >
 						<input type="text" name="ll" value="KKKK" >
 						<button style="float:right; margin-top:15%" onclick="window.location.href = './stickynote.php'">next</button>
 			</form>
@@ -20,10 +16,4 @@
 		
 		exit;
 		}
-	else
-	{
-	//	echo ($_GET);
-	//	echo "---";
-	}
-
 ?>
