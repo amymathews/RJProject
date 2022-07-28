e5ccf701ee24faf17d61ede2e315e4b19ae7bdb6
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-27 22:41:01 -0700

    started on php backend

--- stickynote.php
+++ stickynote.php
@@ -19,28 +19,28 @@
 					<p style="margin-left:18%">actions</p>
 				</div>
 				<div id='stickynotes'></div>
-					<div id='selector' style="position: absolute;font-size:15px;width:20%">
-						<form  method="POST" >
-							<p> <em> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc. </em></p>
-							<p id="note_type"> <strong> Please answer each of the questions below by toggling the drop down box: </strong></p>
-							<label for="notetype">Choose a question</label>
-							<select name="notes" id="notetype">
-								<option value="stakeholder">q3</option>
-								<option value="feeling">q4</option>
-								<option value="action">q5</option>
-							</select>
-							<br><br>
-							<p id="question">  q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm?  </p>
-							<p id="content"> Please input the content you want to display on the note</p>
-							<input id="text_on_note">
-							<br><br>
-							<button id="create" type="button">create new note</button>
-							<p id="remove_note"><strong> Remove: double click on the sticky note to remove it </strong> </p>
-							<button style="float:right; margin-top:15%" onclick="window.location.href = './stickynote.php'">next</button>
-					</div>
-						
-						</form>
-		</div>
+				<div id='selector' style="position: absolute;font-size:15px;width:20%">
+					<form  method="POST" >
+						<p> <em> Reflect on your feeling and experiences, we want you to think about the people who may positively or negatively affect you when gauging the harm caused. These can be the offender, online community members, moderators, etc. </em></p>
+						<p id="note_type"> <strong> Please answer each of the questions below by toggling the drop down box: </strong></p>
+						<label for="notetype">Choose a question</label>
+						<select name="notes" id="notetype">
+							<option value="stakeholder">q3</option>
+							<option value="feeling">q4</option>
+							<option value="action">q5</option>
+						</select>
+						<br><br>
+						<p id="question">  q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm?  </p>
+						<p id="content"> Please input the content you want to display on the note</p>
+						<input id="text_on_note">
+						<br><br>
+						<button id="create" type="button">create new note</button>
+						<p id="remove_note"><strong> Remove: double click on the sticky note to remove it </strong> </p>
+						<button style="float:right; margin-top:15%" onclick="window.location.href = './stickynote.php'">next</button>
+						</div>
+					
+					</form>
+				
 		
 
 			<div class="container" id='pair'>
