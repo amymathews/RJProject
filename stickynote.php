b3ae8bae0819eb3ed63eb8ca421654cc75401246
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-29 15:16:32 -0700

    changing the text of the system

--- stickynote.php
+++ stickynote.php
@@ -33,7 +33,7 @@
 					-->
 					<br><br>
 					<p id="question">   Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm? </p>
-					<p>Please put their names (e.g., offender) into the textbox below, and click "create new note"</p>
+					<p id="question"> Please put their names (e.g., offender) into the textbox below, and click "create new note"</p>
 					<img style="float:right" src="images/Next arrow.png" onclick="clickFunc()">
 					<br><br>
 					<br><br>
