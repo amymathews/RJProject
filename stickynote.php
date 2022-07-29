f42f53c75876f7586f87242d141d8cc3e1a9c035
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-29 16:14:05 -0700

    chnaging the lateout of the system

--- stickynote.php
+++ stickynote.php
@@ -37,7 +37,7 @@
 
 					<br><br>
 					<p id="content"> Please input the content you want to display on the note</p>
-					<input id="text_on_note" style="height: 20px ;">
+					<input id="text_on_note" style="height: 50px ;">
 					<br><br>
 					<button id="create" type="button">create a sticky note</button>
 					<p id="remove_note"><strong> Remove: double click on the sticky note to remove it </strong> </p>
