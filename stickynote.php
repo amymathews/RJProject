f0c730648097ecb64195ee5eed4717e4bc800ede
Amy Mathews <amy.mathews0808@gmail.com>
2022-07-29 16:02:48 -0700

    chnaging the lateout of the system

--- stickynote.php
+++ stickynote.php
@@ -31,7 +31,6 @@
 						<option value="action">q5</option>
 					</select>
 					-->
-					<br>
 					<p id="question"> q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm? <br><br>  Please put their names (e.g., offender) into the textbox below, and click "create new note"</p>
 					<img style="float:right" src=images/next.png onclick="clickFunc()">
 					<img style="float:left" src=images/back.png onclick="backClick()">
