35df7a5dbe08c6a77567be64a86222f62b2f0a5c
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-01 01:20:59 -0700

    adding the backend features

--- stickynote.php
+++ stickynote.php
@@ -11,7 +11,20 @@
 	</head>
 		<body>
 			<form method="POST">
-				<div class = "container">
+				<div class="content">
+					<div class = "container">
+					<div id="category_title" style="position:absolute;margin-left:27%;width:60%;display:flex;font-size:18px">
+						<p style="margin-left:5%">stakeholders</p>
+						<p style="margin-left:17%">feelings</p>
+						<p style="margin-left:18%">desired outcomes</p>
+					</div>
+					<div id='stickynotes'></div>
+            		<div id='selector' style="position: absolute;font-size:15px;width:20%">
+						<p id="note_type"> <strong> On this page, we will work on creating sticky notes to identify the people, feelings and actions related to the harm that you have experienced. </strong></p>
+						<p> <strong>Please answer the questions and we will create the sticky notes for you.</strong></p>
+						<p id="question"> q3) Who do you think can help you address the harm? Who do you think has responsibility for helping you address the harm? <br><br>  Please put their names (e.g., offender) into the textbox below, and click "create new note"</p>
+						<img style="float:right" src=images/next.png onclick="clickFunc()">
+						<img style="float:left" src=images/back.png onclick="backClick()">
 						<br><br>
 						<p id="content"> Please input the content you want to display on the note</p>
 						<input id="text_on_note" name="text_on_note" style="height: 50px ;" />
@@ -19,6 +32,8 @@
 						<button id="create" type="button">create a sticky note</button>
 						<p id="remove_note"><strong> Remove: double click on the sticky note to remove it </strong> </p>
 						<button id="next" name="bu1" type="submit">Next</button>
+					</div>
+					</div>
 				</div>
 			</form>
 		</body>
