6455fee4e075dda38a448e279e44e1f0ce78552b
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-01 01:09:36 -0700

    adding the backend features

--- stickynote.php
+++ stickynote.php
@@ -14,7 +14,7 @@
 					<form method="POST">
 						<br><br>
 						<p id="content"> Please input the content you want to display on the note</p>
-						<input> id="text_on_note" name="text_on_note" style="height: 50px ;"</input>
+						<input id="text_on_note" name="text_on_note" style="height: 50px ;" />
 						<br><br>
 						<button id="create" type="button">create a sticky note</button>
 						<p id="remove_note"><strong> Remove: double click on the sticky note to remove it </strong> </p>
