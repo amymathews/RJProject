0e36738f216a92635157429635519baa52ad0bae
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-03 23:53:42 -0700

    tooltip reset- TAMMY

--- js/StickyNote.js
+++ js/StickyNote.js
@@ -56,7 +56,7 @@ function createwacc () {
             url:"./sninsert.php",    //the page containing php script
             type: "post",    //request type,
             dataType: 'json',
-            data: 'jsonObj',
+            data: {'ab':ab},
             success:function(result){
                 console.log('success');
             },
