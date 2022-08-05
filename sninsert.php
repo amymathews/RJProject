626928d290366f25d340a76ef57ccba03cd35a01
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-04 17:53:56 -0700

    tooltip reset- TAMMY

--- sninsert.php
+++ sninsert.php
@@ -21,8 +21,8 @@
     //if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // if( $_POST["story"] || $_POST["feeling"] ) {
      if( $_POST ) {
-
-        print_r($_POST);
+        print_r("here in php land");
+       // print_r($_POST);
        
     // $sql = "INSERT INTO MyGuests (firstname, lastname, email)VALUES ('John', 'Doe', 'john@example.com')";
    
