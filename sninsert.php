3f38eed1f29da098d37d1da812685174a0cdd546
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-04 21:20:58 -0700

    tooltip reset- TAMMY

--- sninsert.php
+++ sninsert.php
@@ -1,20 +1,23 @@
 <?php
    echo "Hi";
-    //Get Heroku ClearDB connection information
-   //  $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-   //  $cleardb_server = $cleardb_url["host"];
-   //  $cleardb_username = $cleardb_url["user"];
-   //  $cleardb_password = $cleardb_url["pass"];
-   //  $cleardb_db = substr($cleardb_url["path"],1);
-   //  $active_group = 'default';
-   //  $query_builder = TRUE;
-   //  // Connect to DB
-   //  $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+   //  Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-   //      if (!$conn) {
-   //       echo "connection failed";
-   //      die("Connection failed: " . mysqli_connect_error());
-   //  }
+        if (!$conn) {
+         echo "connection failed";
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    {
+      echo "here";
+    }
     
    //  //echo "Connected successfully";
 
