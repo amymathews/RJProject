d873c4b95c0f26d6455a5dd8ce448e1b2d129583
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-07 22:47:59 -0700

    sending value to database

--- js/StickyNote.js
+++ js/StickyNote.js
@@ -28,6 +28,26 @@ jsonObj['userid'];
 jsonObj['stakeholder'] = new Array();
 jsonObj['feeling'] = new Array();
 jsonObj['action'] = new Array();
+var timer;
+var timerStart;
+var timeSpentOnSite = getTimeSpentOnSite();
+
+function getTimeSpentOnSite(){
+    timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
+    timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
+    return timeSpentOnSite;
+}
+
+function startCounting(){
+    timerStart = Date.now();
+    timer = setInterval(function(){
+        timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
+        localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
+        timerStart = parseInt(Date.now());
+        // Convert to seconds
+        console.log(parseInt(timeSpentOnSite/1000));
+    },1000);
+}
 
 // double click deletion function on the sticky note
 function createwacc (counter) {
