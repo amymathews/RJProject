429dd0ee8c56a5296f0ef1843ca13daaec152000
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-08 08:44:25 -0700

    sending value to database

--- information.php
+++ information.php
@@ -10,28 +10,9 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<script>
-    var timer;
-    var timerStart;
-    var timeSpentOnSite = getTimeSpentOnSite();
-
-    function getTimeSpentOnSite(){
-        timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
-        timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
-        return timeSpentOnSite;
-    }
-
-    function startCounting(){
-        alert("Here");
-        timerStart = Date.now();
-        timer = setInterval(function(){
-            timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
-            localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
-            timerStart = parseInt(Date.now());
-            // Convert to seconds
-            console.log(parseInt(timeSpentOnSite/1000));
-        },1000);
-    }
+<script type="text/javascript" src="time.js"></script>
+<script language="javascript">
+   startCounting();
 </script>
 <form action = "<?php $_PHP_SELF ?>" method = "POST">
     <div class="container">
