8847485c10c1fc6ba6638e00437cd275cdee2487
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-08 11:11:33 -0700

    trying to add time

--- time.js
+++ time.js
@@ -1,22 +1,15 @@
-
+var startTime, endTime;
 
 function start() {
-    var startTime;
-    alert("here at start!");
-  startTime = new Date();
-  alert(startTime);
-  end(startTime)
+  startTime = performance.now();
 };
 
-function end(startTime) {
-
-    alert("here at end");
-  endTime = new Date();
-  var timeDiff = endTime.getTime() - startTime.getTime(); //in ms
-  // strip the ms
-  timeDiff /= 1000;
-  alert("this is the timediff", timeDiff);
-
+function end() {
+  endTime = performance.now();
+  var timeDiff = endTime - startTime; //in ms 
+  // strip the ms 
+  timeDiff /= 1000; 
+  
   // get seconds 
   var seconds = Math.round(timeDiff);
   console.log(seconds + " seconds");
