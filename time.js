5066d361a2dc4eb4b2c20a545ede8e2797998316
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-22 11:58:59 -0700

    figuring out time

--- time.js
+++ time.js
@@ -1,24 +1,25 @@
 var timeflag = 0;
 var startTime;
 var result = 0;
-var currmin;
+var currminGlobal;
 // console.log("currmin: " + currmin);
 function start() {
 
 if( timeflag == 0){
   console.log("flag is 0");
   startTime = new Date();
-  currmin = startTime.getMinutes();
+   var currmin = startTime.getMinutes();
+   currminGlobal = currmin;
   console.log("currmin: " + currmin);
 
 //   var currmin = parseInt(startTime.getMinutes());
-  console.log("type: " + typeof(currmin));
+  console.log("type: " + currmin);
   console.log("starttime" + startTime);
   
 }
 
     if( timeflag == 1) {
-        console.log("type: " + typeof(currmin));
+        console.log("curminGlobal: " + currminGlobal);
         console.log("flag is 1");
 
         // let endhour = endTime.getHours();
@@ -27,7 +28,7 @@ if( timeflag == 0){
         // var mintuend = endTime.getMinutes();
         var endTime = new Date();
         var endmin = endTime.getMinutes();
-        result = (endmin - currmin);
+        result = (endmin - currminGlobal);
         console.log("minutes elapsed" + result);
         
     }
