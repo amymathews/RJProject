74596d9f6140912d96e805f99a1c8d46d9d023d1
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-22 03:01:22 -0700

    adding all Jingshu's chnages

--- time.js
+++ time.js
@@ -4,17 +4,16 @@ function start() {
 
 if( timeflag == 0){
   var startTime = performance.now();
-  var minute = startTime.getMinutes();
   console.log("starttime" + startTime);
   
 }
 
     if( timeflag == 1) {
         var endTime = performance.now();
-        let endhour = endTime.getHours();
-        let endmin = endTime.getMinutes();
-        let endsec = endTime.getSeconds();
-        var mintuend = endTime.getMinutes();
+        // let endhour = endTime.getHours();
+        // let endmin = endTime.getMinutes();
+        // let endsec = endTime.getSeconds();
+        // var mintuend = endTime.getMinutes();
         console.log("endtime " + endTime);
         var result = endTime-startTime;
         result /= 1000;
