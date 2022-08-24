54443df1fba9be711028f23754efe27016debb37
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-24 14:08:42 -0700

    added overflow to style.css

--- stickynote.php
+++ stickynote.php
@@ -98,7 +98,7 @@
                     <button id="extend" type="button" style="margin-left:50px">extend timeline</button>
                 </div>
 			</div>
-			<form action = "<?php $_PHP_SELF ?>" method = "POST">
+			<form action = "<?php $_PHP_SELF ?>" style = "overflow:auto" method = "POST">
                 		<button name="complete" style="float:right; margin-top:10%" type="submit" >Complete</button>
 			</form>
 		</div>
