ed2d83a665835cf846a38611ee5921ad1f5fc121
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-24 11:44:43 -0700

    div issues with form tag submission

--- stickynote.php
+++ stickynote.php
@@ -98,9 +98,9 @@
                     <div class="to_right"></div>
                     <button id="extend" type="button" style="margin-left:50px">extend timeline</button>
                 </div>
-				<form action = "<?php $_PHP_SELF ?>" method = "POST">
+				<!-- <form action = "<?php $_PHP_SELF ?>" method = "POST"> -->
                 		<button name="complete" style="float:right; margin-top:5%" type="submit" >Complete</button>
-				</form>
+				<!-- </form> -->
 			</div>
 		</div>
 
