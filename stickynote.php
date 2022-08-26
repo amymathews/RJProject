c4f55d919e231a54fc189f2eb4b0a58cba922db8
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-25 23:06:16 -0700

    backend coordinate

--- stickynote.php
+++ stickynote.php
@@ -127,8 +127,8 @@
 	$wacc = intval($_GET['WACC']);
 	$Elptime = time() - $pastime;
 	$stickyval = $_POST["stickypos"];
-	// orderEntered=$stickyval
-	$sql = "UPDATE heroku_3fa92357decd51e.userdet  SET timeElapsed=$Elptime WHERE userid= $wacc";
+	// 
+	$sql = "UPDATE heroku_3fa92357decd51e.userdet  SET timeElapsed=$Elptime,orderEntered=/"$stickyval/" WHERE userid= $wacc";
 	if (mysqli_query($conn, $sql)) {
 		echo "New record created successfully";
 	 } else {
