4ed1ad302a001604b73ecd1c5d821eaf72e26651
Amy Mathews <amy.mathews0808@gmail.com>
2022-08-26 17:17:30 -0700

    redirect to page

--- stickynote.php
+++ stickynote.php
@@ -1,3 +1,37 @@
+<?php
+  if($_POST){
+	ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+	header("Location: http://www.google.com");
+	 exit();
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+	// Connect to DB
+	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+		if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$pastime = intval($_GET['st']);
+	$wacc = intval($_GET['WACC']);
+	$Elptime = time() - $pastime;
+	$stickyval = $_POST["stickypos"];
+	$sql = "UPDATE heroku_3fa92357decd51e.userdet  SET timeElapsed=$Elptime,orderEntered = '$stickyval' WHERE userid= $wacc";
+	if (mysqli_query($conn, $sql)) {
+		// echo "New record created successfully";
+	 } else {
+		 //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	 }
+	 header("Location: http://www.google.com");
+	 exit();
+  }
+?>
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
@@ -102,34 +136,3 @@
 	<script src="js/StickyNote.js"></script>
 	</body>
 	</html>
-<?php
-  if($_POST){
-	header("Location: http://www.google.com");
-	 exit();
-	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$cleardb_server = $cleardb_url["host"];
-	$cleardb_username = $cleardb_url["user"];
-	$cleardb_password = $cleardb_url["pass"];
-	$cleardb_db = substr($cleardb_url["path"],1);
-	$active_group = 'default';
-	$query_builder = TRUE;
-	// Connect to DB
-	$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-		if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	$pastime = intval($_GET['st']);
-	$wacc = intval($_GET['WACC']);
-	$Elptime = time() - $pastime;
-	$stickyval = $_POST["stickypos"];
-	$sql = "UPDATE heroku_3fa92357decd51e.userdet  SET timeElapsed=$Elptime,orderEntered = '$stickyval' WHERE userid= $wacc";
-	if (mysqli_query($conn, $sql)) {
-		// echo "New record created successfully";
-	 } else {
-		 //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-	 }
-	 header("Location: http://www.google.com");
-	 exit();
-  }
-?>
